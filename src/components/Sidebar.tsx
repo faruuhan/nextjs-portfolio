af1ae73a678ac51d457e0b0afd188e8d15b24a1d
faruuhan <farhnn.r@gmail.com>
2023-10-27 20:57:50 +0700

    fix sticky sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,7 +8,7 @@ export default function Sidebar(): JSX.Element {
   const profile = useSelector((state: RootState) => state.storeSlice.profile);
   return (
     <>
-      <section className='sticky top-0 lg:top-10'>
+      <section className='lg:sticky lg:top-10'>
         <CardProfile data={profile} />
         <NavMenu />
         <h4 className='text-zinc-500 text-center'>
