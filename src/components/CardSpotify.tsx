9bb576a3df208489fc24327406cb09533273bf42
faruuhan <farhnn.r@gmail.com>
2023-10-30 18:12:15 +0700

    Update CardSpotify.tsx

--- src/components/CardSpotify.tsx
+++ src/components/CardSpotify.tsx
@@ -1,34 +1,56 @@
 "use client";
+import { useEffect, useState } from "react";
 import { SiSpotify, SiSpotifyHex } from "@icons-pack/react-simple-icons";
 import useSWR from "swr";
 import Image from "next/image";
+import { Waveform } from "@uiball/loaders";
 
 export default function CardSpotify(): JSX.Element {
   const fetcher = (url: string) => fetch(url).then((ress) => ress.json());
   const { data } = useSWR("api/spotify", fetcher);
+
   return (
     <>
-      <div className='rounded-xl flex gap-2 border p-4 relative bg-white drop-shadow'>
-        {data?.isPlaying ? (
-          <Image
-            height={60}
-            width={60}
-            src={data?.albumImageUrl}
-            alt={data?.album}
-          />
-        ) : (
-          <SiSpotify title='spotify' size={60} color={SiSpotifyHex} />
+      <div className='rounded-xl border bg-white drop-shadow overflow-hidden'>
+        {data?.isPlaying && (
+          <div className='bg-zinc-100 px-4 py-1 flex justify-between items-center'>
+            <p className='text-sm'>Now Playing</p>
+            <Waveform size={16} lineWeight={1.5} color={SiSpotifyHex} />
+          </div>
         )}
-        <div className='whitespace-nowrap overflow-hidden'>
-          <h6 className='font-medium truncate'>
-            {data?.isPlaying ? data.title : "Not Listening"}
-          </h6>
-          <p className='text-sm text-zinc-600'>
-            {data?.isPlaying ? data.artist : "Spotify"}
-          </p>
-        </div>
-        <div className='absolute bottom-4 right-4'>
-          <SiSpotify title='spotify' size={24} color={SiSpotifyHex} />
+        <div className='flex gap-2 p-4 relative'>
+          {data?.isPlaying ? (
+            <a href={data?.songUrl} target='_blank' rel='noopener noreferrer'>
+              <Image
+                height={60}
+                width={60}
+                src={data?.albumImageUrl}
+                alt={data?.album}
+              />
+            </a>
+          ) : (
+            <SiSpotify
+              title={data?.isPlaying && "Spotify is Offline"}
+              size={60}
+              color={data?.isPlaying ? SiSpotifyHex : "#808080"}
+            />
+          )}
+          <div className='whitespace-nowrap overflow-hidden'>
+            <h6 className='font-medium truncate'>
+              {data?.isPlaying ? data.title : "Not Listening"}
+            </h6>
+            <p className='text-sm text-zinc-600 truncate'>
+              {data?.isPlaying ? data.artist : "Spotify"}
+            </p>
+          </div>
+
+          <div className='absolute bottom-4 right-4'>
+            <SiSpotify
+              title={data?.isPlaying ? "Now Playing" : "Spotify is Offline"}
+              size={24}
+              color={data?.isPlaying ? SiSpotifyHex : "#808080"}
+            />
+          </div>
         </div>
       </div>
     </>
